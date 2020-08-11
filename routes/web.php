52362f405b052bb6cfb37bc3053f88fd58981b98
Cris <barcelona1252@gmail.com>
2020-08-11 18:28:22 +0100

    Eliminación de comentarios

--- routes/web.php
+++ routes/web.php
@@ -21,41 +21,33 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-//Como administrador
 Route::middleware(['auth','checkRole:admin'])->group(function(){
     Route::get('/usuariosregistrados','UserController@index')->name('usersIndex');
 });
 
-//Como dueño de mascota y veterinario
 Route::middleware(['auth','checkRole:veterinario,cliente'])->group(function(){
     Route::get('/perfilcliente/{id}','UserController@show')->name('userShow');
     Route::get('/perfilcliente/{id}/edit','UserController@edit')->name('userEdit');
     Route::put('/perfilcliente/{id}','UserController@update')->name('userUpdate');
 });
 
-//Como administrador, dueño de mascota y veterinario
 Route::middleware(['auth','checkRole:admin,veterinario,cliente'])->group(function(){
     Route::delete('/perfil/{id}','UserController@destroy')->name('userDestroy');
 });
 
-//Como veterinario
 Route::middleware(['auth','checkRole:veterinario'])->group(function(){
     Route::get('/clientes','VetClientController@index')->name('clientIndex');
     Route::get('/clientes/create','VetClientController@create')->name('createClient');
     Route::post('/clientes','VetClientController@store')->name('storeClient');
     Route::delete('/clientes/{id}','VetClientController@destroy')->name('deleteClient');
-    // Como veterinario
     Route::get('/informacionmascota/{id}/edit', 'PetController@edit')->name('editPetInfo');
     Route::put('/informacionmascota/{id}','PetController@update')->name('updatePetInfo');
 });
 
-
-//Como dueño de mascota y veterinario
 Route::middleware(['auth','checkRole:veterinario,cliente'])->group(function(){
     Route::get('/historialmedico/{id}','MedicalFileController@index')->name('medicalhistoryIndex');
 });
 
-//Como veterinario
 Route::middleware(['auth','checkRole:veterinario'])->group(function(){
     Route::get('/historialmedico/{id}/create','MedicalFileController@create')->name('createFile');
     Route::post('/historialmedico/{id}','MedicalFileController@store')->name('storeFile');
@@ -64,30 +56,26 @@ Route::middleware(['auth','checkRole:veterinario'])->group(function(){
     Route::delete('/historialmedico/{idficha}/{id}','MedicalFileController@destroy')->name('deleteFile');
 });
 
-//Como dueño de mascota y veterinario
 Route::middleware(['auth','checkRole:veterinario,cliente'])->group(function(){
     Route::get('/imagenesficha/{idficha}/{idmascota}','MedicalFileImageController@index')->name('indexImage');
 });
 
-//Como veterinario
 Route::middleware(['auth','checkRole:veterinario'])->group(function(){
     Route::get('/imageneshistorialmedico/{idficha}/create','MedicalFileImageController@create')->name('createImages');
     Route::post('/imageneshistorialmedico/{idficha}','MedicalFileImageController@store')->name('storeImages');
 });
 
-//Como dueño de mascota
 Route::middleware(['auth','checkRole:cliente'])->group(function(){
     Route::get('/vacunas','VaccineController@indexclient')->name('vaccineClientIndex');
 });
 
-//Como veterinario
 Route::middleware(['auth','checkRole:veterinario'])->group(function(){
     Route::get('/vacunas/{id}','VaccineController@index')->name('vaccineIndex');
     Route::get('/vacunas/{id}/create','VaccineController@create')->name('createVaccine');
     Route::post('/vacunas/{id}','VaccineController@store')->name('storeVaccine');
     Route::delete('/vacunas/{idvacuna}/{id}','VaccineController@destroy')->name('deleteVaccine');
 });
-//Como dueño de mascota y veterinario
+
 Route::middleware(['auth','checkRole:veterinario,cliente'])->group(function(){
     Route::get('/citas','AppointmentController@index')->name('appointmentsIndex');
     Route::get('/citas/create','AppointmentController@create')->name('createAppointment');
@@ -97,7 +85,6 @@ Route::middleware(['auth','checkRole:veterinario,cliente'])->group(function(){
     Route::delete('/citas/{idcita}','AppointmentController@destroy')->name('deleteAppointment');
 });
 
-//Como veterinario
 Route::middleware(['auth','checkRole:veterinario'])->group(function(){
     Route::get('/tareas','TaskController@index')->name('indexTasks');
     Route::get('/proximastareas','TaskController@nextTasksindex')->name('indexNextTasks');
