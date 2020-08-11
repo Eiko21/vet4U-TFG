f68739b858583b13993f400a0f54fb480e21aa35
Cris <barcelona1252@gmail.com>
2020-08-11 19:01:07 +0100

    Middleware recuperado

---
+++ app/Http/Middleware/CheckUserRole.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckUserRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $roleIds = ['admin' => 1, 'veterinario' => 2, 'cliente' => 3];
+        $allowedRoleIds = [];
+        foreach ($roles as $role)
+        {
+           if(isset($roleIds[$role]))
+           {
+               $allowedRoleIds[] = $roleIds[$role];
+           }
+        }
+        $allowedRoleIds = array_unique($allowedRoleIds); 
+
+        if(Auth::check()) {
+          if(in_array(Auth::user()->role_id, $allowedRoleIds)) {
+            return $next($request);
+          }
+        }
+        return $next($request);
+    }
+}
