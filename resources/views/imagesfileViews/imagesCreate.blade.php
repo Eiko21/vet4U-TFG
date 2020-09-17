d69ac84e8a91b22b6888d5991f3c00b9c2450cb9
Cris <barcelona1252@gmail.com>
2020-09-17 18:13:48 +0100

    Nuevo estilo de la galería de imágenes de cada ficha

--- resources/views/imagesfileViews/imagesCreate.blade.php
+++ resources/views/imagesfileViews/imagesCreate.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.basic')
 
 @section('styles')
-<link href="{{ asset('css/medicalHistoryStyle.css') }}" rel="stylesheet">
+<link href="{{ asset('css/responsive-design/newImageStyle.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
-<div class="new-file">
-    <div style="text-align: center">
-        <h2>Selección de imágenes</h2>
+<div class="newImage-content">
+    <div class="title-section"><h2>Selección de imágenes</h2></div>
+    <div class="image-form">
+        <table class="tableFormImage">
+            <form action="{{ route('storeImages', ['idficha' => $idficha, 'idmascota' => $idmascota]) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <tr>
+                    <td><label for="imagenes">Seleccione las imágenes a subir</label></td>
+                    <td><input type="file" name="imagen"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" class="upload-img" name="uploadImages" value="Subir imagen"></td>
+                </tr>
+            </form>
+                <tr>
+                    <td>
+                        <form action="{{ route('indexImage', ['idficha' => $idficha, 'idmascota' => $idmascota]) }}" method="GET">
+                            @csrf
+                            <input type="submit" class="cancel-btn" name="cancelfile" value="Cancelar">
+                        </form>
+                    </td>
+                </tr>
+        </table>
     </div>
-    <table>
-        <form action="{{ route('storeImages', ['idficha' => $idficha, 'idmascota' => $idmascota]) }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <tr>
-                <td><label for="imagenes">Seleccione las imágenes a subir</label></td>
-                <td><input type="file" name="imagen"></td>
-            </tr>
-            <tr>
-                <td><input type="submit" name="uploadImages" value="Subir imagen"></td>
-            </tr>
-        </form>
-            <tr>
-                <td>
-                    <form action="{{ route('indexImage', ['idficha' => $idficha, 'idmascota' => $idmascota]) }}" method="GET">
-                        @csrf
-                        <input type="submit" class="cancel-btn" name="cancelfile" value="Cancelar">
-                    </form>
-                </td>
-            </tr>
-    </table>
 </div>
 @endsection
--- resources/views/imagesfileViews/imagesIndex.blade.php
+++ resources/views/imagesfileViews/imagesIndex.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.basic')
 
 @section('styles')
-<link href="{{ asset('css/galeryStyle.css') }}" rel="stylesheet">
+<link href="{{ asset('css/responsive-design/fileImagesStyle.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
 <div class="images-content">
-    @if ($imagenes->count() > 0)
-        <div class="images-section">
-            @foreach ($imagenes as $imagen)
-                <img src="{{ asset('images/'.$imagen->imagen) }}" width="120" height="120">
-            @endforeach
+    <div class="title-button-section">
+        <div id="return-section">
+            <form class="returnForm" action="{{ route('medicalhistoryIndex', ['id' => $idmascota]) }}" method="GET">
+                @csrf
+                <input type="submit" class="return-btn" name="returnToMH" value="Volver al historial">
+            </form>
         </div>
-    @else
-        <div class="empty-section">
-            <p>Esta ficha no tiene imágenes</p>
-        </div>
-    @endif   
-    <div id="return-section">
-        {{-- <form class="returnForm" action="{{ route('petMedicalHistoryIndex', ['clientid' => $clientid]) }}" method="GET"> --}}
-        <form class="returnForm" action="{{ route('medicalhistoryIndex', ['id' => $idmascota]) }}" method="GET">
-            @csrf
-            <input type="submit" class="return-btn" name="returnToMH" value="Volver al historial">
-        </form>
+        <h2>Imágenes</h2>
+    </div>
+    <div class="images-section">
+        @if ($imagenes->count() > 0)
+                @foreach ($imagenes as $imagen)
+                    <img src="{{ asset('images/'.$imagen->imagen) }}" width="200" height="200">
+                @endforeach
+        @else
+                <p>No hay imágenes.</p>
+        @endif   
     </div>
 </div>
 @endsection
