559ed268bca504f3e376b24d9e525ce6192fb80c
Cris <barcelona1252@gmail.com>
2020-09-20 13:02:31 +0100

    Corrección en el estilo del perfil de usuario

--- resources/views/userViews/userEdit.blade.php
+++ resources/views/userViews/userEdit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.basic')
 
 @section('styles')
-    <link href="{{ asset('css/responsive-design/editUserProfileStyle.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive-design/editProfileStyle.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
